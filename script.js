c9db36c3ffa437b5f1b4613bd1c34de2d077cbee
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-09-05 17:04:17 +0300

    This commit gets the value of the month and day

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function getName(){
     let year = document.getElementById("year-entry").value;
+    let month= Number(document.getElementById("month-entry").value);
+    let day= Number(document.getElementById("day-entry").value);
 
     
 
