cc81ba29b2d1f0caef44e9f74f4a1dd8b32e7edc
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-09-05 17:08:56 +0300

    This commit gets the values of the genders from the form

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ function getName(){
     let month= Number(document.getElementById("month-entry").value);
     let day= Number(document.getElementById("day-entry").value);
 
+    let genders = document.getElementsByName("gender")
+
+    
+
     
 
 }
