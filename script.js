10c43c9e82c889b5e607f95b4608adfb48d502ea
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-09-05 22:34:42 +0300

    This commit adds the day and month checker to ensure days do not exeed 31 and months don't exceed 12

--- script.js
+++ script.js
@@ -18,18 +18,30 @@ function getName() {
         }
     }
     let extractedGender = findGender();
-    
+
     console.log(extractedGender);
 
     function validMonth() {
         if (month < 1 || month > 12) {
             return false;
-        } else{
+        } else {
             return true;
         }
     }
 
+    function validDay() {
+        if (day < 1 || day > 31) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+
     let monthChecker = validMonth();
+    let dayChecker = validDay();
+
 
     let dayOfWeekNumber = Math.floor((((Number(year.slice(0, 2)) / 4) - 2 * Number(year.slice(0, 2)) - 1) +
         ((5 * Number(year.slice(2, 4)) / 4)) + ((26 * (month + 1) / 10)) + day) % 7);
@@ -41,11 +53,14 @@ function getName() {
         index = dayOfWeekNumber - 1;
     }
 
-    if (extractedGender == "male") {
+    if (extractedGender == "male" && monthChecker && dayChecker) {
         document.getElementById('output-results').textContent = "You were born on a " + daysOfWeek[index] + " , your Akan name is " + maleNames[index];
         return false;
-    }else{
-        alert("Loser")
+    } else if (extractedGender == "female" && monthChecker) {
+        document.getElementById('output-results').textContent = "You were born on a " + daysOfWeek[index] + ", your Akan name is" + femaleNames[index];
+        return false;
+    } else {
+        alert("Please enter valid month and date")
     }
 
 
