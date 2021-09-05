acda5b08fa1552e8a69eab126258c7be419a0b65
Ckhajira <christopher.khajira@student.moringaschool.com>
2021-09-05 17:13:36 +0300

    This commit adds the male and female akan names in their respective arrays

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-function getName(){
+function getName() {
     let year = document.getElementById("year-entry").value;
-    let month= Number(document.getElementById("month-entry").value);
-    let day= Number(document.getElementById("day-entry").value);
+    let month = Number(document.getElementById("month-entry").value);
+    let day = Number(document.getElementById("day-entry").value);
 
-    let genders = document.getElementsByName("gender")
-
-    
-
-    
+    let genders = document.getElementsByName("gender");
+    let maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    let femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
 }
